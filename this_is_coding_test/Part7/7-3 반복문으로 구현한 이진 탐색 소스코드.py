3735beb0524007f228c21236b453e59fa9d49b7c
kimth007kim <kimth007kim@naver.com>
2021-01-29 18:33:24 +0900

    이것이 코딩테스트다 7장 7-3 이진탐색코드(반복문) 까지 끝

---
+++ this_is_coding_test/Part7/7-3 반복문으로 구현한 이진 탐색 소스코드.py
@@ -0,0 +1,21 @@
+# 이진 탐색 소스코드 구현(반복문)
+def binary_search(array,target,start,end):
+    while start<= end:
+        mid = (start+ end) //2
+        if array[mid]== target:
+            return mid
+        elif array[mid]>target:
+            end=mid-1
+        else:
+            start = mid+1
+    return None
+
+n,target = map(int,input().split())
+array= list(map(int,input().split()))
+
+result = binary_search(array,target,0,n-1)
+
+if result ==None:
+    print("찾는게 없어")
+else:
+    print(result+1)
