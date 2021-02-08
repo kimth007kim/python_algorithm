614a8f0b792fa59b936e5b460cc5994debcfe90f
kimth007kim <kimth007kim@naver.com>
2021-02-08 16:34:28 +0900

    이것이 코딩테스트다 8장 다이나믹 프로그래밍 연습문제2 개미전사두번째 끝

--- this_is_coding_test/Part8/8-6 개미 전사.py
+++ this_is_coding_test/Part8/8-6 개미 전사.py
@@ -4,8 +4,10 @@ array= list(map(int,input().split()))
 d = [0]*101
 d[0]=array[0]
 d[1] =max(array[0],array[1])
-print(d[1])
 for i in range(2,n):
+    print("i=",i,"d[i-1]=",d[i-1],"d[i-2]+array[i]=",d[i-2]+array[i])
     d[i]=max(d[i-1],d[i-2]+array[i])
+    print("d[i]로 결정",d[i])
 
 print(d[n-1])
+print(d)
