6f583129f2a2cd837652209b21bb6d8ae1ecb34d
kimth007kim <kimth007kim@naver.com>
2021-02-20 20:49:51 +0900

    이것이 코딩테스트다 9장 최단 경로 9-2완료 6,7번째

--- bakjoon/난이도별/Chap14 동적 계획법/1932.py
+++ bakjoon/난이도별/Chap14 동적 계획법/1932.py
@@ -6,7 +6,7 @@ for i in range(n):
 
 # print(graph[1][0])
 d[1][1]=graph[1][0]
-for i in range(2,n+1):
-    for j in range(n+1):
-        d[i][j]= d[i-]
+# for i in range(2,n+1):
+#     for j in range(n+1):
+#         d[i][j]= d[i-]
 print(d)
