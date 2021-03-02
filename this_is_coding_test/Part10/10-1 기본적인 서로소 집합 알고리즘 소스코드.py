5733576a30a46df74d6af7d365bfb655de7a6fa9
kimth007kim <kimth007kim@naver.com>
2021-03-02 20:18:30 +0900

    10-4 서로소 집합을 활용한 사이클 판별 소스코드

--- this_is_coding_test/Part10/10-1 기본적인 서로소 집합 알고리즘 소스코드.py
+++ this_is_coding_test/Part10/10-1 기본적인 서로소 집합 알고리즘 소스코드.py
@@ -4,12 +4,14 @@ def find_parent(parent,x):
     return x
 
 def union_parent(parent,a,b):
+    print(a,b)
     a=find_parent(parent,a)
     b=find_parent(parent,b)
     if a < b:
         parent[b]=a
     else:
         parent[a]=b
+    print(parent)
 
 v,e =map(int,input().split())
 parent=[0] *(v+1)
@@ -21,7 +23,7 @@ for i in range(e):
     a,b=map(int,input().split())
     union_parent(parent,a,b)
 
-print(' 각 원소가 속한 집합:',end='')
+print('각 원소가 속한 집합:',end='')
 for i in range(1,v+1):
     print(find_parent(parent,i),end=' ')
 print()
