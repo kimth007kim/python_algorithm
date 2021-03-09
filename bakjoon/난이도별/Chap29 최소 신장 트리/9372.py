6b9fe8edcb5b24076cb0acb970392a47300152e6
kimth007kim <kimth007kim@naver.com>
2021-03-09 17:58:57 +0900

    백준 29장 최소 신장트리 9372 완료

---
+++ bakjoon/난이도별/Chap29 최소 신장 트리/9372.py
@@ -0,0 +1,11 @@
+import sys
+
+input= sys.stdin.readline
+t= int(input())
+
+for i in range(t):
+    n,m= map(int,input().split())
+    for j in range(m):
+        a,b= map(int,input().split())
+
+    print(n-1)
