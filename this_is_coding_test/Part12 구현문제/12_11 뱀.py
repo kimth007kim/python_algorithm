7d71b1c545d3c07174c7fadd357b45110b8a4e63
DESKTOP-F0F1JFN\김경동 <kimth007kim@naver.com>
2021-03-28 20:39:17 +0900

    이것이 코딩테스트다 12장 구현문제 뱀 해답

--- this_is_coding_test/Part12 구현문제/12_11 뱀.py
+++ this_is_coding_test/Part12 구현문제/12_11 뱀.py
@@ -11,3 +11,68 @@
 # 3 D       3초후에 오른쪽으로회전
 # 15 L
 # 17 D
+
+# 6
+# 3
+# 3 4
+# 2 5
+# 5 3
+# 3
+# 3 D
+# 15 L
+# 17 D
+
+n=int(input())
+k=int(input())
+data = [[0] * (n+1) for _ in range(n+1)]
+info=[]
+
+for _ in range(k):
+    a,b = map(int,input().split())
+
+l= int(input())
+for _ in range(l):
+    x,c=input().split()
+    info.append((int(x),c))
+
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
+
+def turn(direction,c):
+    if c == "L":
+        direction = (direction-1) %4
+    else:
+        direction = (direction+1) %4
+    return direction
+
+def simulate():
+    x,y=1,1
+    data[x][y]=2
+    direction=0
+    time=0
+    index=0
+    q=[(x,y)]
+    while True:
+        nx= x+ dx[direction]
+        ny= y+ dy[direction]
+        if 1<= nx and nx <= n and 1<=ny and data[nx][ny]!=2:
+            if data[nx][ny]==0:
+                data[nx][ny]=2
+                q.append((nx,ny))
+                px,py=q.pop(0)
+                data[px][py]=0
+            if data[nx][ny]==1:
+                data[nx][ny]=2
+                q.aapend((nx,ny))
+        else:
+            time+=1
+            break
+        x,y= nx,ny
+        time+=1
+        if index <1 and time ==info[index][0]:
+            direction =turn(direction,info[index][1])
+            index+=1
+    return time
+
+print(simulate())
+
