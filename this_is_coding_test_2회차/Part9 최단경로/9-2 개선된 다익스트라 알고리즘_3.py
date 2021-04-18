4cbd62b718ffd4c135d21061cf72f9a8e093466a
kimth007kim <kimth007kim@naver.com>
2021-04-18 21:31:13 +0900

    이것이 코딩테스트다 2회차 9장 최단경로 다익스트라,플로이드워셜

---
+++ this_is_coding_test_2회차/Part9 최단경로/9-2 개선된 다익스트라 알고리즘_3.py
@@ -0,0 +1,35 @@
+import heapq
+
+INF=int(1e9)
+n,m=map(int,input().split())
+start=int(input())
+graph=[[] for _ in range(n+1)]
+distance=[INF]*(n+1)
+
+for i in range(m):
+    a,b,c=map(int,input().split())
+    graph[a].append((b,c))
+
+def dajikstra(start):
+    q=[]
+    heapq.heappush(q,(0,start))
+    distance[start]=0
+    while q:
+        dist,now= heapq.heappop(q)
+        if distance[now]<dist:
+            continue
+        for i in graph[now]:
+            cost= dist+i[1]
+            if cost< distance[i[0]]:
+                distance[i[0]]=cost
+                heapq.heappush(q,(cost,i[0]))
+
+
+dajikstra(start)
+
+
+for i in range(1,n+1):
+    if distance[i]==INF:
+        print("infinity")
+    else:
+        print(distance[i])
