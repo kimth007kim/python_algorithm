a1169ace314dfb1b36f0a41c5401f9e915d9a3fc
kimth007kim <kimth007kim@naver.com>
2021-05-31 16:54:58 +0900

    이것이 코딩테스트다 2회차 13장_2 BFSDFS 연산자 끼워넣기 완료

--- this_is_coding_test_2회차/Part13 DFSBFS_2/13-19 연산자 끼워넣기.py
+++ this_is_coding_test_2회차/Part13 DFSBFS_2/13-19 연산자 끼워넣기.py
@@ -6,42 +6,42 @@ word+=str(number[0])
 max_value=-1e9
 min_value=1e9
 
-def calc(cnt,word):
+def calc(cnt,now):
     global add,sub,mul,div,min_value,max_value
     if cnt==n:
-        temp1=word[:1]
-        temp2=word[1:]
-        answer=""
-        result=0
-        for i in range(0,len(temp2)+1,2):
-            # print(i)
-            # print(temp1,"temp2[:i]=",temp2[:i])
-            temp1+= temp2[i:i+2]
-            result=int(eval(temp1))
-            temp1=str(result)
-        min_value=min(min_value,result)
-        max_value=max(max_value,result)
+        # temp1=word[:1]
+        # temp2=word[1:]
+        # answer=""
+        # result=0
+        # for i in range(0,len(temp2)+1,2):
+        #     # print(i)
+        #     # print(temp1,"temp2[:i]=",temp2[:i])
+        #     temp1+= temp2[i:i+2]
+        #     result=int(eval(temp1))
+        #     temp1=str(result)
+        min_value=min(min_value,now)
+        max_value=max(max_value,now)
         return
 
     if add>0:
         add-=1
-        calc(cnt+1,word+"+"+str(number[cnt]))
+        calc(cnt+1,now+number[cnt])
         add+=1
     if sub>0:
         sub-=1
-        calc(cnt+1,word+"-"+str(number[cnt]))
+        calc(cnt+1,now-number[cnt])
         sub+=1
     if mul>0:
         mul-=1
-        calc(cnt+1,word+"*"+str(number[cnt]))
+        calc(cnt+1,now*number[cnt])
         mul+=1
     if div>0:
         div-=1
-        calc(cnt+1,word+"/"+str(number[cnt]))
+        calc(cnt+1,int(now/number[cnt]))
         div+=1
 
 
 
-calc(1,word)
+calc(1,number[0])
 print(max_value)
 print(min_value)
