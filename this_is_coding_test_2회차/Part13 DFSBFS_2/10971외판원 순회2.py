8c428cb3e17964dd680e86f87f5b3dc239b7e907
kimth007kim <kimth007kim@naver.com>
2021-06-06 21:12:05 +0900

    이것이 코딩테스트다 2회차 13장_2 BFSDFS 백준 10971외판원순회2 시간초과로 실패

---
+++ this_is_coding_test_2회차/Part13 DFSBFS_2/10971외판원 순회2.py
@@ -0,0 +1,30 @@
+n=int(input())
+graph=[]
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+
+number=[1]*n
+
+min_value=10**7
+
+def dfs(cnt,start,prev,result):
+    global min_value,number
+    if cnt==n-1:
+        if graph[prev][start]==0:
+            return
+        else:
+            answer=result+graph[prev][start]
+            # print(answer)
+            min_value=min(min_value,answer)
+        return
+    for j in range(n):
+        if graph[prev][j]!=0 and number[j]!=0:
+            number[j]=0
+            dfs(cnt+1,start,j,result+graph[prev][j])
+            number[j]=1
+
+for i in range(n):
+    number[i]=0
+    dfs(0,i,i,0)
+    number[i] = 1
+print(min_value)
