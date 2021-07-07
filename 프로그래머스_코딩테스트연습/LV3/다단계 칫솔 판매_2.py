6f198b6a7befc0ecd959d33bcd4b8344ae098f9d
kimth007kim <kimth007kim@naver.com>
2021-07-07 18:41:20 +0900

    프로그래머스 LV3 다단계 칫솔 판매_2 완료

--- 프로그래머스_코딩테스트연습/LV3/다단계 칫솔 판매_2.py
+++ 프로그래머스_코딩테스트연습/LV3/다단계 칫솔 판매_2.py
@@ -1,13 +1,10 @@
 def find(parents, money, number, answer):
-    print(parents, money, number, answer)
-    print("parents[number]  =", parents[number])
-    if parents[number] == number or money // 10 == 0:
+    if money // 10 == 0 or parents[number] == number:
         answer[number] += money
         return
     send = money // 10
     mine = money - send
     answer[number] += mine
-    print("number,answer[number]", number, answer[number])
     find(parents, send, parents[number], answer)
     return
 
@@ -18,24 +15,19 @@ def solution(enroll, referral, seller, amount):
     print("seller", seller)
     print("amount", amount)
     n = len(enroll)
-    answer = [0] * (n + 1)
-    d = {}
     parents = [i for i in range(n + 1)]
-    print("초기parents= ", parents)
+    d = {}
     for i in range(n):
         d[enroll[i]] = i + 1
-    print("d          = ", d)
-    # print(referral)
     for i in range(n):
         if referral[i] == "-":
             parents[i + 1] = 0
         else:
             parents[i + 1] = d[referral[i]]
-    # print(seller)
-    print("변경parents= ", parents)
+    answer = [0] * (n + 1)
     for i in range(len(seller)):
-        print(amount[i])
-        print(d[seller[i]])
-
         find(parents, amount[i] * 100, d[seller[i]], answer)
+        # print(seller[i],amount[i])
+
+    # print(answer)
     return answer[1:]
