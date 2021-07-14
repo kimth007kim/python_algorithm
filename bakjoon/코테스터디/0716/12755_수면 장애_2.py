f355c0a78f0019b4e49ce97f750a59658f81c841
kimth007kim <kimth007kim@naver.com>
2021-07-14 17:07:57 +0900

    코테 스터디  12755_수면장애 완료

--- bakjoon/코테스터디/0716/12755_수면 장애_2.py
+++ bakjoon/코테스터디/0716/12755_수면 장애_2.py
@@ -5,33 +5,38 @@ def digit(n):
     differ=0
     for i in range(0,10):
         big=9*(i+1)*(10**i)
-        # print(differ,"   ",n," ",differ+big)
         if differ<=n<=differ+big:
-            return i+1,differ,differ+big
+            return i,differ,differ+big
         else:
             differ+=big
-        # if 9*(10**i)<n<9*(10**(i+1)):
-        #     return i+2
 
 n=int(input)
 d,start,end=digit(n)
+# print(d,start,end)
+
 # print("&&&&&&&&&&&&&&",n-start-1)
-NEWNUM=start+(n-start+1)//d
-print(NEWNUM)
+NEWNUM= (10**d)+(n-start-1)//(d+1)
+# start+=1
+# NEWNUM=start+(n-start+1)//d
+# print(NEWNUM)
 word=str(NEWNUM)
 # print("답답답답답답답답답",word[(n-start-1)%d])
-if 1<=n<=9:
-    print(n)
-else:
-    print(word[(n-start-1)%d])
+# if 1<=n<=9:
+#     print(n)
+# else:
+#     print(word[(n-start-1)%(d+1)])
+print(word[(n-start-1)%(d+1)])
 
-result=n-start
-print(d,start,end)
-s=""
-print("--------",n-start)
-while len(s)<result:
-    start += 1
-    s+=str(start)
-print(s)
-print(len(s))
-print(s[result-1])
+
+
+# result=n-start
+# print(d,start,end)
+# s=""
+# print("--------",n-start)
+#
+# while len(s)<result:
+#     start += 1
+#     s+=str(start)
+# print(s)
+# print(len(s))
+# print(s[result-1])
