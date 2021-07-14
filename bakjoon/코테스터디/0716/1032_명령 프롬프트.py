bcfea2bdccde7c0a71abc09d6fb483d291933fd9
kimth007kim <kimth007kim@naver.com>
2021-07-14 17:16:01 +0900

    코테 스터디 1032명령프롬프트 완료

--- bakjoon/코테스터디/0716/1032_명령 프롬프트.py
+++ bakjoon/코테스터디/0716/1032_명령 프롬프트.py
@@ -1,11 +1,26 @@
-# n= int(input())
-# data=[]
-# for i in range(n):
-#     data.append(input())
-n=3
-data=["config.sys","config.inf","configures"]
+import sys
+input=sys.stdin.readline
 
-cnt=0
+n= int(input())
+data=[]
+for i in range(n):
+    data.append(input())
+# n=3
+# data=["config.sys","config.inf","configures"]
+
+result=""
 for j in range(len(data[0])):
+    cnt = 0
     for i in range(n):
-        print(data[i][j])
+        if i==0:
+            tmp=data[i][j]
+        else:
+            if data[i][j]==tmp:
+                cnt+=1
+            else:
+                break
+    if cnt==n-1:
+        result+=(data[i][j])
+    else:
+        result+="?"
+print(result)
