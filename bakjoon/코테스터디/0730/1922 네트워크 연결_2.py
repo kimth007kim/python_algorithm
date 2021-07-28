df0be20c1a54207c2185a4a62a9f28fa3557a7e1
kimth007kim <kimth007kim@naver.com>
2021-07-28 19:53:30 +0900

    코테 스터디-07/31-1922 네트워크 연결 완료

---
+++ bakjoon/코테스터디/0730/1922 네트워크 연결_2.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent,x):
+    if parent[x] !=x:
+        parent[x]= find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a= find_parent(parent,a)
+    b= find_parent(parent,b)
+    if a> b:
+        parent[a]=b
+    else:
+        parent[b]=a
+
+
+
+n=int(input())  #컴퓨터의 수
+m=int(input())  #간선의 수
+
+parent=[0]*(n+1)
+for i in range(1,n+1):
+    parent[i]=i
+
+edges=[]
+result=0
+for i in range(m):
+    a,b,cost=map(int,input().split())
+    edges.append((cost,a,b))
+
+edges.sort()
+
+for edge in edges:
+    cost,a,b=edge
+    if find_parent(parent,a) != find_parent(parent,b):
+        # print(parent)
+        union_parent(parent,a,b)
+        result+=cost
+
+print(result)
