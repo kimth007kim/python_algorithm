32d26e30289e4ef9b401805764a1a9b41cd06cec
kimth007kim <kimth007kim@naver.com>
2021-11-21 18:41:55 +0900

    파이썬으로 배우는 자료구조 핵심원리  / 7장 트리 / 7-1 트리 완료

---
+++ 파이썬으로 배우는 자료구조 핵심원리/7장 트리/7_1 tree.py
@@ -0,0 +1,57 @@
+class TreeNode:
+    def __init__(self, data=None):
+        self.__data=data
+        self.__left=None
+        self.__right=None
+
+    def __del__(self):
+        print('data {} is deleted'.format(self.__data))
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        self.__data=data
+
+    @property
+    def left(self):
+        return self.__left
+
+    @left.setter
+    def left(self, left):
+        self.__left=left
+
+    @property
+    def right(self):
+        return self.__right
+
+    @right.setter
+    def right(self, right):
+        self.__right=right
+
+def preorder(cur):
+    if not cur:
+        return
+    print(cur.data,end=" ")
+    preorder(cur.left)
+    preorder(cur.right)
+
+# def iter
+
+if __name__=="__main__":
+    n1= TreeNode(1)
+    n2= TreeNode(2)
+    n3= TreeNode(3)
+    n4= TreeNode(4)
+    n5= TreeNode(5)
+    n6= TreeNode(6)
+    n7= TreeNode(7)
+
+    n1.left=n2; n1.right=n3
+    n2.left=n4; n2.right=n5
+    n3.left=n6; n3.right=n7
+
+    preorder(n1)
+    print()
