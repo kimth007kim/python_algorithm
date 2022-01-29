6961ed62eca09466646efded0fbd4c2e0ac2a319
kimth007kim <kimth007kim@naver.com>
2022-01-29 22:01:36 +0900

    코테 스터디 - 0130 -11725 트리의 부모찾기 복습_2

---
+++ 코테스터디/2022/0130/11725 트리의 부모찾기_2.py
@@ -0,0 +1,30 @@
+
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+n= int(input())
+
+tree = [[] for _ in range(n+1)]
+parents=[0 for _ in range(n+1)]
+
+print(tree)
+print(parents)
+
+for _ in range(n-1):
+    a,b= map(int,input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+print(tree)
+print(parents)
+
+def dfs(start,tree,parents):
+    print(start,tree,parents)
+    for i in tree[start]:
+        if parents[i]== 0:
+            parents[i]=start
+            dfs(i,tree,parents)
+
+dfs(1,tree,parents)
