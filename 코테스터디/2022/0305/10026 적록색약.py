81cbd4754287d78728af54ce22309b14af2154cc
kimth007kim <kimth007kim@naver.com>
2022-03-02 23:54:51 +0900

    코테 스터디 - 0305 - 10026 적록 색약 완료

--- 코테스터디/2022/0305/10026 적록색약.py
+++ 코테스터디/2022/0305/10026 적록색약.py
@@ -3,14 +3,28 @@ from collections import deque
 n = int(input())
 graph=[]
 visited=[[False]*n for _ in range(n)]
-new_graph=[[False]*n for _ in range(n)]
-new_visited=[]
+new_graph=[]
+new_visited=[[False]*n for _ in range(n)]
 dx=[1,0,-1,0]
 dy=[0,1,0,-1]
 
-def bfs(graph,visit,x,y):
+def bfs(graph,visit,i,j):
+    visit[i][j]=True
+    check=graph[i][j]
     q=deque()
+    q.append([i,j])
+    while q:
+        x,y= q.popleft()
+        for i in range(4):
+            nx= dx[i]+x
+            ny= dy[i]+y
+            if 0<=nx<n and 0<=ny<n and graph[nx][ny]==check:
+                if visit[nx][ny]==False:
+                    visit[nx][ny]=True
+                    q.append([nx,ny])
 
+cnt_0=0
+cnt_1=0
 for _ in range(n):
     arr=list(input())
     tmp=[]
@@ -26,7 +40,9 @@ for i in range(n):
     for j in range(n):
         if not visited[i][j]:
             bfs(graph,visited,i,j)
-            if not visited[i][j]:
-                bfs(graph, visited, i, j)
-            print(i,j,graph[i][j])
+            cnt_0+=1
+        if not new_visited[i][j]:
+            bfs(new_graph, new_visited, i, j)
+            cnt_1+=1
 
+print(cnt_0,cnt_1)
