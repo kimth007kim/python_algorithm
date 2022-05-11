408b765efb29bb87cd906a9626e848a5f415b193
kimth007kim <kimth007kim@naver.com>
2022-05-11 15:15:27 +0900

    코테 스터디 - 0508-  20444 색종이와 가위 해설

---
+++ 코테스터디/2022/0508/20444 색종이와 가위.py
@@ -0,0 +1,18 @@
+n,k = map(int,input().split())
+
+def solution(lt,rt):
+    while lt<rt:
+        mid = (lt+rt)//2
+        value = (mid+1)*(n-mid+1)
+        print(mid,value)
+        if value == k:
+            print("YES")
+            return
+        if value>k:
+            rt =mid -1
+        else:
+            lt= mid+1
+    print("NO")
+    return
+
+solution(0,n)
