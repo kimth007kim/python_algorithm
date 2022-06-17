a2367f9d487e3956e1d7f102675e17ff924ff27c
kimth007kim <kimth007kim@naver.com>
2022-06-17 21:17:50 +0900

    코테 스터디 - 0619/0616/14728 벼락치기 해설

---
+++ 코테스터디/2022/0619/0616/14728 벼락치기 해설.py
@@ -0,0 +1,20 @@
+n,t = map(int,input().split())
+time, score = [], []
+
+for i in range(n):
+    K, S = map(int, input().split())
+    time.append(K)
+    score.append(S)
+
+print(time,score)
+
+dp = [[0 for _ in range(t+1)] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,t+1):
+        if j>= time[i-1]:
+            dp[i][j]=max(dp[i-1][j],dp[i-1][j-time[i-1]]+score[i-1])
+        else:
+            dp[i][j]=dp[i-1][j]
+
+print(dp[n][t])
