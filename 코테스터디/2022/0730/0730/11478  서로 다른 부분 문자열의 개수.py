d8e72dbc0c152c916cea43dc7bfcf23d1c4937f1
kimth007kim <kimth007kim@naver.com>
2022-07-30 21:47:16 +0900

    코테 스터디 - 0730/0730/ 11478 서로 다른 부분 문자열의 개수 완료

---
+++ 코테스터디/2022/0730/0730/11478  서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+
+s = input().rstrip()
+
+
+result=set()
+for i in range(len(s)):
+    for j in range(i,len(s)):
+        result.add(s[i:j+1])
+
+print(len(result))
