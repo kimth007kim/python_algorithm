897b0ed3c86ae3948ff5ecfb756ef24a26f54210
kimth007kim <kimth007kim@naver.com>
2022-10-30 22:06:20 +0900

    코테 스터디 -1030/1030/ 5569 출근 경로 해설

---
+++ 코테스터디/2022/1030/1030/5569 출근 경로.py
@@ -0,0 +1,20 @@
+w,h = map(int,input().split())
+
+dp = [[[[0 for _ in range(2)] for _ in range(2)] for _ in range(w+1)] for _ in range(h+1)]
+
+for i in range(2,h+1):
+    dp[i][1][0][0] = 1
+for i in range(2,w+1):
+    dp[1][i][0][1] = 1
+
+
+for i in dp:
+    print(i)
+for i in range(2,h+1):
+    for j in range(2,w+1):
+        dp[i][j][0][0] = (dp[i-1][j][0][0] + dp[i-1][j][1][0])%100000
+        dp[i][j][0][1] = (dp[i][j-1][0][1] + dp[i][j-1][1][1])%100000
+        dp[i][j][1][0] = (dp[i-1][j][0][1])%100000
+        dp[i][j][1][1] = (dp[i][j-1][0][0])%100000
+
+print((dp[h][w][0][0] + dp[h][w][1][0] + dp[h][w][0][1] + dp[h][w][1][1])%100000)
