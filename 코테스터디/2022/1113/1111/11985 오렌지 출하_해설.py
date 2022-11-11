0855a7445f170e41951bcc09448ec64baa05f532
kimth007kim <kimth007kim@naver.com>
2022-11-11 17:08:33 +0900

    코테 스터디 -1113/1111/ 11985 오렌지 출하 해설

---
+++ 코테스터디/2022/1113/1111/11985 오렌지 출하_해설.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+
+a = [0] + [int(input()) for _ in range(n)]
+
+dp = [0] * (n + 1)
+dp[1] = k
+
+for i in range(2, n + 1):
+    min_v = max_v = a[i]
+    print(min_v, max_v)
+    dp[i] = dp[i - 1] + k
+    for siz in range(2, min(m, i) + 1):
+        j = i - siz + 1
+        print(j)
+        min_v, max_v = min(min_v, a[j]), max(max_v, a[j])
+        box_cost = k + siz * (max_v + min_v)
+        dp[i] = min(dp[i], dp[j - 1] + box_cost)
+        print(min_v, max_v)
+print(dp[n])
